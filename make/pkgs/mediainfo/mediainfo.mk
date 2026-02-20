6a3a0e47412e5368d128e0766fc61bc740e8f281
fda77 <fda77@users.noreply.github.com>
2026-02-20 22:38:29 +0100

    p7zip: use FREETZ_RPATH  - closes https://github.com/Freetz-NG/freetz-ng/pull/1433

--- make/pkgs/mediainfo/mediainfo.mk
+++ make/pkgs/mediainfo/mediainfo.mk
@@ -9,18 +9,12 @@ $(PKG)_DIR:=$(SOURCE_DIR)/MediaInfo-$($(PKG)_VERSION)
 ### CVSREPO:=https://github.com/MediaArea/MediaInfo
 ### SUPPORT:=Ircama
 
-ZENLIB_VERSION:=0.4.41
 MEDIAINFOLIB_VERSION:=25.10
-
-ZENLIB_SOURCE:=ZenLib-v$(ZENLIB_VERSION).tar.gz
-ZENLIB_SITE:=https://github.com/MediaArea/ZenLib/archive/refs/tags
-ZENLIB_HASH:=45d5173fa0278f5264daa6836ae297aa303984482227d00b35c4f03929494c8f
 
 MEDIAINFOLIB_SOURCE:=MediaInfoLib-v$(MEDIAINFOLIB_VERSION).tar.gz
 MEDIAINFOLIB_SITE:=https://github.com/MediaArea/MediaInfoLib/archive/refs/tags
 MEDIAINFOLIB_HASH:=e4b2b82f3df8d2c190643d1705ee35c3102674954858d02a2e2b42840f0f07aa
 
-ZENLIB_DIR:=$(SOURCE_DIR)/ZenLib-$(ZENLIB_VERSION)
 MEDIAINFOLIB_DIR:=$(SOURCE_DIR)/MediaInfoLib-$(MEDIAINFOLIB_VERSION)
 
 # Intermediate variables to avoid double expansion in shell commands
@@ -32,16 +26,14 @@ MEDIAINFO_PKG_HASH := $($(PKG)_HASH)
 $(PKG)_BINARY:=$($(PKG)_DIR)/Project/GNU/CLI/mediainfo
 $(PKG)_BINARY_TARGET:=$($(PKG)_DEST_DIR)/usr/bin/mediainfo
 
-$(PKG)_LIBZEN_BINARY:=$(ZENLIB_DIR)/Project/GNU/Library/.libs/libzen.so.0.0.0
-$(PKG)_LIBZEN_STAGING_LIB:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libzen.so.0.0.0
-$(PKG)_LIBZEN_TARGET_LIB:=$($(PKG)_TARGET_LIBDIR)/libzen.so.0.0.0
-
 $(PKG)_LIBMEDIAINFO_BINARY:=$(MEDIAINFOLIB_DIR)/Project/GNU/Library/.libs/libmediainfo.so.0.0.0
 $(PKG)_LIBMEDIAINFO_STAGING_LIB:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libmediainfo.so.0.0.0
 $(PKG)_LIBMEDIAINFO_TARGET_LIB:=$($(PKG)_TARGET_LIBDIR)/libmediainfo.so.0.0.0
 
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-shared
+
+$(PKG)_DEPENDS_ON += libzen
 
 # Download MediaInfo source.
 # Upstream asset name is "v<version>.tar.gz", but we store it locally using a unique
@@ -51,37 +43,11 @@ $(DL_DIR)/$(MEDIAINFO_PKG_SOURCE): | $(DL_DIR)
 
 $(PKG_UNPACKED)
 
-# Download additional sources
-$(DL_DIR)/ZenLib-v$(ZENLIB_VERSION).tar.gz: | $(DL_DIR)
-	$(DL_TOOL) -o ZenLib-v$(ZENLIB_VERSION).tar.gz $(DL_DIR) v$(ZENLIB_VERSION).tar.gz $(ZENLIB_SITE) $(ZENLIB_HASH)
-
 $(DL_DIR)/MediaInfoLib-v$(MEDIAINFOLIB_VERSION).tar.gz: | $(DL_DIR)
 	$(DL_TOOL) -o MediaInfoLib-v$(MEDIAINFOLIB_VERSION).tar.gz $(DL_DIR) v$(MEDIAINFOLIB_VERSION).tar.gz $(MEDIAINFOLIB_SITE) $(MEDIAINFOLIB_HASH)
 
-# Build ZenLib
-$($(PKG)_LIBZEN_BINARY): $(DL_DIR)/ZenLib-v$(ZENLIB_VERSION).tar.gz
-	$(call UNPACK_TARBALL,$<,$(SOURCE_DIR))
-	(cd $(ZENLIB_DIR)/Project/GNU/Library && \
-		./autogen.sh && \
-		$(TARGET_CONFIGURE_ENV) \
-		./configure \
-			--host=$(GNU_TARGET_NAME) \
-			--build=$(GNU_HOST_NAME) \
-			--prefix=/usr \
-			--enable-static \
-			--enable-shared && \
-		$(SUBMAKE) \
-	) $(SILENT)
-
-$($(PKG)_LIBZEN_STAGING_LIB): $($(PKG)_LIBZEN_BINARY)
-	$(SUBMAKE) -C $(ZENLIB_DIR)/Project/GNU/Library \
-		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		install
-	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libzen.la
-
 # Build MediaInfoLib
-$($(PKG)_LIBMEDIAINFO_BINARY): $(DL_DIR)/MediaInfoLib-v$(MEDIAINFOLIB_VERSION).tar.gz $($(PKG)_LIBZEN_STAGING_LIB)
+$($(PKG)_LIBMEDIAINFO_BINARY): $(DL_DIR)/MediaInfoLib-v$(MEDIAINFOLIB_VERSION).tar.gz
 	$(call UNPACK_TARBALL,$<,$(SOURCE_DIR))
 	(cd $(MEDIAINFOLIB_DIR)/Project/GNU/Library && \
 		./autogen.sh && \
@@ -129,9 +95,6 @@ $(MEDIAINFO_BINARY): $(MEDIAINFO_DIR)/.configured
 $(MEDIAINFO_BINARY_TARGET): $(MEDIAINFO_BINARY)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_LIBZEN_TARGET_LIB): $($(PKG)_LIBZEN_STAGING_LIB)
-	$(INSTALL_LIBRARY_STRIP)
-
 $($(PKG)_LIBMEDIAINFO_TARGET_LIB): $($(PKG)_LIBMEDIAINFO_STAGING_LIB)
 	$(INSTALL_LIBRARY_STRIP)
 
@@ -142,22 +105,19 @@ $(pkg)-precompiled: $($(PKG)_BINARY_TARGET)
 ifeq ($(strip $(FREETZ_PACKAGE_MEDIAINFO_STATIC)),y)
 $(pkg)-precompiled:
 else
-$(pkg)-precompiled: $($(PKG)_LIBZEN_TARGET_LIB) $($(PKG)_LIBMEDIAINFO_TARGET_LIB)
+$(pkg)-precompiled: $($(PKG)_LIBMEDIAINFO_TARGET_LIB)
 endif
 
 $(pkg)-clean:
-	-[ -d $(ZENLIB_DIR)/Project/GNU/Library ] && $(MAKE) -C $(ZENLIB_DIR)/Project/GNU/Library clean $(SILENT)
 	-[ -d $(MEDIAINFOLIB_DIR)/Project/GNU/Library ] && $(MAKE) -C $(MEDIAINFOLIB_DIR)/Project/GNU/Library clean $(SILENT)
 	-[ -d $($(PKG)_DIR)/Project/GNU/CLI ] && $(MAKE) -C $($(PKG)_DIR)/Project/GNU/CLI clean $(SILENT)
 	$(RM) -r \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libzen* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libmediainfo* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/ZenLib \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/MediaInfo* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libzen.pc \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libmediainfo.pc
 
 $(pkg)-uninstall:
-	$(RM) $(MEDIAINFO_BINARY_TARGET) $(MEDIAINFO_LIBZEN_TARGET_LIB) $(MEDIAINFO_LIBMEDIAINFO_TARGET_LIB)
+	$(RM) $(MEDIAINFO_BINARY_TARGET) $(MEDIAINFO_LIBMEDIAINFO_TARGET_LIB)
 
 $(PKG_FINISH)
+
