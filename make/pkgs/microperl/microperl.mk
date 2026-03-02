5c38c46fdd5847c7e6e2d93643b2334874233d4a
Ircama <Ircama>
2026-03-02 04:54:03 +0100

    microperl: fix # in PATCH_POST_CMDS causing Make comment truncation
    In GNU Make, # inside variable values is treated as a comment
    character, so:
      echo -e '\n#ifndef PerlProc_pipe_cloexec...' >> iperlsys.h
    was silently truncated to:
      echo -e '\n
    ...causing a bash syntax error on the next line.
    
    Fix: move the PerlProc_pipe_cloexec macro addition to a proper
    patch file (412-add-pipe-cloexec-stub.patch) which is applied
    via the existing conditional patches mechanism for 5.38.

--- make/pkgs/microperl/microperl.mk
+++ make/pkgs/microperl/microperl.mk
@@ -21,7 +21,6 @@ $(PKG)_PATCH_PRE_CMDS += chmod -R u+w .;
 $(PKG)_PATCH_POST_CMDS += $(if $(FREETZ_PACKAGE_MICROPERL_VERSION_5_38),cp -f $(abspath $(dir $(lastword $(MAKEFILE_LIST)))patches/5.38/uuudmap.h) $(abspath $(dir $(lastword $(MAKEFILE_LIST)))patches/5.38/ubitcount.h) $(abspath $(dir $(lastword $(MAKEFILE_LIST)))patches/5.38/umg_data.h) .;)
 ifeq ($(FREETZ_PACKAGE_MICROPERL_VERSION_5_38),y)
 $(PKG)_PATCH_POST_CMDS += echo -e '\n/*\n * Stub implementations for functions not available in PERL_MICRO\n */\n\nvoid\nPerl_optimize_optree(pTHX_ OP *o)\n{\n    /* No-op in microperl */\n}\n\nvoid\nPerl_finalize_optree(pTHX_ OP *o)\n{\n    /* No-op in microperl */\n}\n\nvoid\nPerl_peep(pTHX_ OP *o)\n{\n    /* No-op in microperl */\n}\n\nvoid\nPerl_rpeep(pTHX_ OP *o)\n{\n    /* No-op in microperl */\n}' >> op.c;
-$(PKG)_PATCH_POST_CMDS += echo -e '\n#ifndef PerlProc_pipe_cloexec\n#ifdef HAS_PIPE2\n#define PerlProc_pipe_cloexec(fd)	pipe2((fd), O_CLOEXEC)\n#else\n#define PerlProc_pipe_cloexec(fd)	pipe(fd)\n#endif\n#endif' >> iperlsys.h;
 endif
 $(PKG)_PATCH_POST_CMDS += $(if $(FREETZ_PACKAGE_MICROPERL_VERSION_5_38),echo -e '\nsub register_categories {\n    # Stub for microperl\n}' >> lib/warnings.pm;)
 $(PKG)_PATCH_POST_CMDS += $(if $(FREETZ_PACKAGE_MICROPERL_VERSION_5_38),echo -e '\nsub enabled {\n    # Stub for microperl - always return 0\n    return 0;\n}\nsub warn {\n    # Stub for microperl\n}\nsub warnif {\n    # Stub for microperl\n}' >> lib/warnings.pm;)
