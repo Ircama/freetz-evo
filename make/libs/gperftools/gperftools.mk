e82069edf2e077b60fa95cbd3085d4863d43df58
Ircama <Ircama>
2026-03-17 23:58:26 +0100

    Add gperftools and jemalloc

---
+++ make/libs/gperftools/gperftools.mk
@@ -0,0 +1,87 @@
+$(call PKG_INIT_LIB, 2.18.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=d18d919175f9e4d740ace6b52f0f4f91284160c454e91b36ffd6456282a02206
+$(PKG)_SITE:=https://github.com/gperftools/gperftools/releases/download/$(pkg)-$($(PKG)_VERSION)
+### WEBSITE:=https://gperftools.github.io/gperftools/
+### MANPAGE:=https://gperftools.github.io/gperftools/tcmalloc.html
+### CHANGES:=https://github.com/gperftools/gperftools/releases
+### CVSREPO:=https://github.com/gperftools/gperftools
+
+# libtool version-info current:revision:age
+# TCMALLOC_SO_VERSION=10:5:6  -> soname = current-age = 4
+# PROFILER_SO_VERSION=5:19:5  -> soname = current-age = 0
+$(PKG)_TCMALLOC_LIB_VERSION:=4.6.5
+$(PKG)_PROFILER_LIB_VERSION:=0.5.19
+
+$(PKG)_TCMALLOC_MINIMAL_BINARY:=$($(PKG)_DIR)/.libs/libtcmalloc_minimal.so.$($(PKG)_TCMALLOC_LIB_VERSION)
+$(PKG)_TCMALLOC_MINIMAL_STAGING:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libtcmalloc_minimal.so.$($(PKG)_TCMALLOC_LIB_VERSION)
+$(PKG)_TCMALLOC_MINIMAL_TARGET:=$($(PKG)_TARGET_DIR)/libtcmalloc_minimal.so.$($(PKG)_TCMALLOC_LIB_VERSION)
+
+$(PKG)_PROFILER_BINARY:=$($(PKG)_DIR)/.libs/libprofiler.so.$($(PKG)_PROFILER_LIB_VERSION)
+$(PKG)_PROFILER_STAGING:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libprofiler.so.$($(PKG)_PROFILER_LIB_VERSION)
+$(PKG)_PROFILER_TARGET:=$($(PKG)_TARGET_DIR)/libprofiler.so.$($(PKG)_PROFILER_LIB_VERSION)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_ENV += ac_cv_have_pthread_setspecific_with___thread=no
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-debugalloc
+$(PKG)_CONFIGURE_OPTIONS += --disable-heap-checker
+$(PKG)_CONFIGURE_OPTIONS += --disable-heap-profiler
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_LIB_libprofiler),--enable-cpu-profiler,--disable-cpu-profiler)
+$(PKG)_CONFIGURE_OPTIONS += --enable-minimal
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_TCMALLOC_MINIMAL_BINARY) $($(PKG)_PROFILER_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(GPERFTOOLS_DIR)
+
+$($(PKG)_TCMALLOC_MINIMAL_STAGING): $($(PKG)_TCMALLOC_MINIMAL_BINARY)
+	$(SUBMAKE) -C $(GPERFTOOLS_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	@if [ -f "$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libtcmalloc_minimal.pc" ]; then \
+		$(PKG_FIX_LIBTOOL_LA) \
+			$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libtcmalloc_minimal.pc; \
+	fi
+	@if [ -f "$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libprofiler.pc" ]; then \
+		$(PKG_FIX_LIBTOOL_LA) \
+			$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libprofiler.pc; \
+	fi
+
+ifeq ($(strip $(FREETZ_LIB_libprofiler)),y)
+$($(PKG)_PROFILER_STAGING): $($(PKG)_TCMALLOC_MINIMAL_STAGING)
+	@test -f "$@"
+endif
+
+$($(PKG)_TCMALLOC_MINIMAL_TARGET): $($(PKG)_TCMALLOC_MINIMAL_STAGING)
+	$(INSTALL_LIBRARY_STRIP)
+
+$($(PKG)_PROFILER_TARGET): $($(PKG)_PROFILER_STAGING)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_TCMALLOC_MINIMAL_STAGING) $(if $(FREETZ_LIB_libprofiler),$($(PKG)_PROFILER_STAGING))
+
+$(pkg)-precompiled: $($(PKG)_TCMALLOC_MINIMAL_TARGET) $(if $(FREETZ_LIB_libprofiler),$($(PKG)_PROFILER_TARGET))
+
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(GPERFTOOLS_DIR) clean
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libtcmalloc* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libprofiler* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libtcmalloc*.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libprofiler*.pc
+	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/gperftools
+
+$(pkg)-uninstall:
+	$(RM) \
+		$(GPERFTOOLS_TARGET_DIR)/libtcmalloc*.so* \
+		$(GPERFTOOLS_TARGET_DIR)/libprofiler*.so*
+
+$(PKG_FINISH)
---
+++ make/libs/jemalloc/jemalloc.mk
@@ -0,0 +1,65 @@
+$(call PKG_INIT_LIB, 5.3.0)
+$(PKG)_LIB_VERSION:=2
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_HASH:=2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aecfeaa
+$(PKG)_SITE:=https://github.com/jemalloc/jemalloc/releases/download/$($(PKG)_VERSION)
+### WEBSITE:=https://jemalloc.net/
+### MANPAGE:=https://jemalloc.net/jemalloc.3.html
+### CHANGES:=https://github.com/jemalloc/jemalloc/releases
+### CVSREPO:=https://github.com/jemalloc/jemalloc
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/lib/libjemalloc.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libjemalloc.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/libjemalloc.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-static
+$(PKG)_CONFIGURE_OPTIONS += --disable-debug
+$(PKG)_CONFIGURE_OPTIONS += --disable-doc
+$(PKG)_CONFIGURE_OPTIONS += --disable-fill
+$(PKG)_CONFIGURE_OPTIONS += --disable-prof
+$(PKG)_CONFIGURE_OPTIONS += --disable-stats
+$(PKG)_CONFIGURE_OPTIONS += --disable-experimental
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(JEMALLOC_DIR) \
+		build_lib_shared
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(JEMALLOC_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install_lib_shared install_include install_bin
+	@if [ -f "$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/jemalloc.pc" ]; then \
+		$(PKG_FIX_LIBTOOL_LA) \
+			$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/jemalloc.pc; \
+	fi
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(JEMALLOC_DIR) clean
+	$(RM) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libjemalloc* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/jemalloc.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/jemalloc-config \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/jemalloc.sh \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/jeprof
+	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/jemalloc
+
+$(pkg)-uninstall:
+	$(RM) $(JEMALLOC_TARGET_DIR)/libjemalloc.so*
+
+$(PKG_FINISH)
