c26059bfeb4f331d8153f407c5de8a5010ad623d
Ircama <Ircama>
2026-03-30 15:35:42 +0200

    Update dosfstools

--- make/pkgs/dosfstools/dosfstools.mk
+++ make/pkgs/dosfstools/dosfstools.mk
@@ -1,32 +1,30 @@
-$(call PKG_INIT_BIN, v3.0.28)
+$(call PKG_INIT_BIN, 4.2)
 $(PKG)_CATEGORY:=Disk Tools
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_HASH:=93b5581c5fd223cc5a3d7de2a567083e4fac9e2b416489119a1a51b6c42be5ce
-$(PKG)_SITE:=git://github.com/dosfstools/dosfstools.git
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=64926eebf90092dca21b14259a5301b7b98e7b1943e8a201c7d726084809b527
+$(PKG)_SITE:=https://github.com/dosfstools/dosfstools/releases/download/v$($(PKG)_VERSION)
 
 $(PKG)_BINARIES_ALL := fsck.fat fatlabel mkfs.fat
 $(PKG)_BINARIES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_ALL))
-$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARIES_BUILD_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/src/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
 $(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
 
 ifeq ($(strip $(FREETZ_TARGET_UCLIBC_0_9_28)),y)
 $(PKG)_DEPENDS_ON += iconv
-$(PKG)_LIBS += -liconv
+$(PKG)_CONFIGURE_ENV += LIBS="-liconv"
 endif
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_NOP)
+$(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(DOSFSTOOLS_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -fomit-frame-pointer" \
-		LDFLAGS="$(DOSFSTOOLS_LIBS)" \
+		V=1 \
 		all
 
-$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/%
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/sbin/%: $($(PKG)_DIR)/src/%
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
