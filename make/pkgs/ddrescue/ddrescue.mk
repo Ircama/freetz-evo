e0e6f734c73f1f412a55d726154114ca2302205a
Ircama <Ircama>
2026-04-07 11:56:15 +0200

    Add ddrescue

---
+++ make/pkgs/ddrescue/ddrescue.mk
@@ -0,0 +1,40 @@
+$(call PKG_INIT_BIN, 1.30)
+$(PKG)_CATEGORY:=Disk Clone
+$(PKG)_SOURCE:=gddrescue_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_HASH:=d01c9ff0599a13d10261c9b435333cc4aaff016be226788cfe515329c221789a
+$(PKG)_SITE:=https://deb.debian.org/debian/pool/main/g/gddrescue
+$(PKG)_DIR:=$(SOURCE_DIR)/gddrescue-$($(PKG)_VERSION)
+### WEBSITE:=https://www.gnu.org/software/ddrescue/
+### MANPAGE:=https://www.gnu.org/software/ddrescue/manual/ddrescue_manual.html
+### CHANGES:=https://ftp.gnu.org/gnu/ddrescue/
+
+$(PKG)_BINARIES:=ddrescue ddrescuelog
+$(PKG)_BINARIES_BUILD_DIR:=$(addprefix $($(PKG)_DIR)/,$($(PKG)_BINARIES))
+$(PKG)_BINARIES_TARGET_DIR:=$(addprefix $($(PKG)_DEST_DIR)/usr/bin/,$($(PKG)_BINARIES))
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DDRESCUE_DIR) V=1 all \
+		CXX="$(TARGET_CXX)" \
+		CPPFLAGS="$(TARGET_CFLAGS)" \
+		CXXFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)"
+
+$(foreach binary,$($(PKG)_BINARIES_BUILD_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/bin)))
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	@if [ -f "$(DDRESCUE_DIR)/Makefile" ]; then \
+		$(SUBMAKE) -C $(DDRESCUE_DIR) clean; \
+	fi
+
+$(pkg)-uninstall:
+	$(RM) $(DDRESCUE_BINARIES:%=$(DDRESCUE_DEST_DIR)/usr/bin/%)
+
+$(PKG_FINISH)
