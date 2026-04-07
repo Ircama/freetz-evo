2ccc68db5c396868d35885a978b870a295231428
Ircama <Ircama>
2026-04-07 11:57:46 +0200

    Add udpcast

---
+++ make/pkgs/udpcast/udpcast.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 20250223)
+$(PKG)_CATEGORY:=Disk Clone
+$(PKG)_SOURCE:=udpcast_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_HASH:=cefd7554c877e1bc76987d2b96b23f7699a2e5340c254454f61b6e0dae370aa7
+$(PKG)_SITE:=https://deb.debian.org/debian/pool/main/u/udpcast
+$(PKG)_DIR:=$(SOURCE_DIR)/udpcast-$($(PKG)_VERSION)
+### WEBSITE:=https://udpcast.linux.lu/
+### MANPAGE:=https://manpages.debian.org/udpcast
+### CHANGES:=https://tracker.debian.org/pkg/udpcast
+
+$(PKG)_BINARIES:=udp-sender udp-receiver
+$(PKG)_BINARIES_BUILD_DIR:=$(addprefix $($(PKG)_DIR)/,$($(PKG)_BINARIES))
+$(PKG)_BINARIES_TARGET_DIR:=$(addprefix $($(PKG)_DEST_DIR)/usr/sbin/,$($(PKG)_BINARIES))
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(UDPCAST_DIR) V=1 all
+
+$(foreach binary,$($(PKG)_BINARIES_BUILD_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/sbin)))
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(UDPCAST_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(UDPCAST_BINARIES:%=$(UDPCAST_DEST_DIR)/usr/sbin/%)
+
+$(PKG_FINISH)
