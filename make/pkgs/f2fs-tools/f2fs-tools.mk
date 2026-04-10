7729e9396e6f9464ed387bfc90a78797110f213b
Ircama <Ircama>
2026-04-11 01:30:02 +0200

    Add f2fs-tools

---
+++ make/pkgs/f2fs-tools/f2fs-tools.mk
@@ -0,0 +1,48 @@
+$(call PKG_INIT_BIN, 1.9.0)
+$(PKG)_CATEGORY:=Disk Tools
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=77217562ae7011a6d81b7b3c43c42623db1796a57596408d6c8037def70d6cc7
+$(PKG)_SITE:=https://github.com/jaegeuk/f2fs-tools/archive/refs/tags/v$($(PKG)_VERSION)
+
+$(PKG)_BINARIES_ALL := mkfs.f2fs fsck.f2fs
+$(PKG)_BINARIES := $(call PKG_SELECTED_SUBOPTIONS,$($(PKG)_BINARIES_ALL))
+
+# libtool places real ELF binaries in .libs/ subdirectories
+$(PKG)_BIN_MKFS_F2FS  := $($(PKG)_DIR)/mkfs/.libs/mkfs.f2fs
+$(PKG)_BIN_FSCK_F2FS  := $($(PKG)_DIR)/fsck/.libs/fsck.f2fs
+$(PKG)_BINARIES_BUILD_ALL := \
+	$($(PKG)_BIN_MKFS_F2FS) \
+	$($(PKG)_BIN_FSCK_F2FS)
+
+$(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/sbin/%)
+$(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)/usr/sbin/%,$(filter-out $($(PKG)_BINARIES),$($(PKG)_BINARIES_ALL)))
+
+$(PKG)_DEPENDS_ON += e2fsprogs
+$(PKG)_CONFIGURE_PRE_CMDS += $(AUTORECONF)
+$(PKG)_CONFIGURE_ENV += ac_cv_search_uuid_generate="-luuid"
+$(PKG)_CONFIGURE_ENV += ac_cv_file__git=no
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARIES_BUILD_ALL): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(F2FS_TOOLS_DIR) V=1 all
+
+$($(PKG)_DEST_DIR)/usr/sbin/mkfs.f2fs: $($(PKG)_BIN_MKFS_F2FS)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_DEST_DIR)/usr/sbin/fsck.f2fs: $($(PKG)_BIN_FSCK_F2FS)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(F2FS_TOOLS_DIR) clean
+
+$(pkg)-uninstall:
+	$(RM) $(F2FS_TOOLS_BINARIES_ALL:%=$(F2FS_TOOLS_DEST_DIR)/usr/sbin/%)
+
+$(PKG_FINISH)
