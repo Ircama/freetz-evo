a164e1d443871d49dd79b1a80e58a0b0c1e4241a
Ircama <Ircama>
2026-05-17 17:35:18 +0200

    Fix ffmpeg mpd-cgi mpd mympd-cgi

--- make/pkgs/ffmpeg/ffmpeg.mk
+++ make/pkgs/ffmpeg/ffmpeg.mk
@@ -39,7 +39,7 @@ $(PKG)_LIBS_TARGET_DIR     := $($(PKG)_LIBNAMES_LONG:%=$($(PKG)_TARGET_LIBDIR)/%
 $(PKG)_ENCODERS  := ac3 jpegls mjpeg mpeg1video mpeg2video mpeg4 pcm_s16be pcm_s16le png tiff vorbis zlib
 $(PKG)_DECODERS  := aac ac3 atrac3 gif h264 jpegls libopenjpeg mjpeg mjpegb mp2 mp3 mpeg1video mpeg2video mpeg4 mpegvideo pcm_s16be pcm_s16le png tiff vorbis wmav1 wmav2 zlib
 $(PKG)_MUXERS    := ac3 avi flv h264 matroska mjpeg mov mp3 mp4 mpeg1video mpeg2video mpegts ogg rtp
-$(PKG)_DEMUXERS  := ac3 avi flv h264 image2 matroska mjpeg mov mp3 mpegps mpegts mpegvideo ogg rm rtsp sdp v4l2
+$(PKG)_DEMUXERS  := aac ac3 avi flv h264 hls image2 matroska mjpeg mov mp3 mpegps mpegts mpegvideo ogg rm rtsp sdp v4l2
 $(PKG)_PARSERS   := aac ac3 h264 mjpeg mpegaudio mpegvideo mpeg4video
 $(PKG)_PROTOCOLS := file http https pipe rtp tcp udp
 
