5c6dfb745c1a9a8c9b67fecce480021d27df4830
Ircama <Ircama>
2026-05-30 23:33:55 +0200

    openldap: add package wiring updates

--- make/pkgs/openldap/openldap.mk
+++ make/pkgs/openldap/openldap.mk
@@ -1,5 +1,5 @@
 $(call PKG_INIT_BIN, 2.6.8)
-$(PKG)_LIB_VERSION:=2.200.0
+$(PKG)_LIB_VERSION:=2.0.200
 $(PKG)_SOURCE:=openldap-$($(PKG)_VERSION).tgz
 $(PKG)_HASH:=48969323e94e3be3b03c6a132942dcba7ef8d545f2ad35401709019f696c3c4e
 $(PKG)_SITE:=https://www.openldap.org/software/download/OpenLDAP/openldap-release
@@ -14,8 +14,13 @@ $(PKG)_LIBS_BUILD_DIR := \
 $(PKG)_LIBS_STAGING_DIR := $(addprefix $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/,$($(PKG)_LIBS))
 $(PKG)_LIBS_TARGET_DIR := $(addprefix $($(PKG)_TARGET_LIBDIR)/,$($(PKG)_LIBS))
 
+$(PKG)_BINARIES := ldapsearch ldapmodify ldapadd ldapdelete ldapwhoami
+$(PKG)_BINARIES_STAGING_DIR := $(addprefix $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/,$($(PKG)_BINARIES))
+$(PKG)_BINARIES_TARGET_DIR := $(addprefix $($(PKG)_DEST_DIR)/usr/bin/,$($(PKG)_BINARIES))
+
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_liblber
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libldap
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENLDAP
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
@@ -25,6 +30,7 @@ $(PKG)_CONFIGURE_OPTIONS += --enable-overlays=no
 $(PKG)_CONFIGURE_OPTIONS += --enable-slapd=no
 $(PKG)_CONFIGURE_OPTIONS += --with-cyrus-sasl=no
 $(PKG)_CONFIGURE_OPTIONS += --with-tls=no
+$(PKG)_CONFIGURE_OPTIONS += --with-yielding_select=yes
 
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -32,6 +38,8 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/include
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/libraries/liblutil
 	$(SUBMAKE) -C $(OPENLDAP_DIR)/libraries/liblber
 	$(SUBMAKE) -C $(OPENLDAP_DIR)/libraries/libldap
 
@@ -55,9 +63,20 @@ $($(PKG)_LIBS_STAGING_DIR): $($(PKG)_LIBS_BUILD_DIR)
 $($(PKG)_LIBS_TARGET_DIR): $($(PKG)_TARGET_LIBDIR)/%: $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%
 	$(INSTALL_LIBRARY_STRIP_WILDCARD_BEFORE_SO)
 
+$($(PKG)_BINARIES_STAGING_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/libraries/liblber
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/libraries/libldap
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/clients/tools
+	$(SUBMAKE) -C $(OPENLDAP_DIR)/clients/tools \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		STRIP_OPTS= \
+		install
+
+$(foreach binary,$($(PKG)_BINARIES_STAGING_DIR),$(eval $(call INSTALL_BINARY_STRIP_RULE,$(binary),/usr/bin)))
+
 $(pkg): $($(PKG)_LIBS_STAGING_DIR)
 
-$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR)
+$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $(if $(FREETZ_PACKAGE_OPENLDAP),$($(PKG)_BINARIES_TARGET_DIR))
 
 
 $(pkg)-clean:
@@ -70,7 +89,7 @@ $(pkg)-clean:
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/etc/ldap*.conf*
 
 $(pkg)-uninstall:
-	$(RM) $(OPENLDAP_TARGET_LIBDIR)/libldap*.so* $(OPENLDAP_TARGET_LIBDIR)/liblber*.so*
+	$(RM) $(OPENLDAP_TARGET_LIBDIR)/libldap*.so* $(OPENLDAP_TARGET_LIBDIR)/liblber*.so* $(OPENLDAP_DEST_DIR)/usr/bin/{ldapsearch,ldapmodify,ldapadd,ldapdelete,ldapwhoami}
 
 $(call PKG_ADD_LIB,liblber)
 $(call PKG_ADD_LIB,libldap)
