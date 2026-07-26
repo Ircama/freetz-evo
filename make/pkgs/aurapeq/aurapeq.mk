36d2503e1ec68a5c9c14e760ee74b8dbd392b486
Ircama <Ircama>
2026-07-26 11:21:17 +0200

    Fix hidapi and aurapeq

--- make/pkgs/aurapeq/aurapeq.mk
+++ make/pkgs/aurapeq/aurapeq.mk
@@ -11,6 +11,9 @@ $(PKG)_DEPENDS_ON += libwebsockets
 $(PKG)_BINARY_BUILD:=$($(PKG)_DIR)/aura-bridged
 $(PKG)_BINARY_TARGET:=$($(PKG)_DEST_DIR)/usr/bin/aura-bridged
 
+$(PKG)_BINARY_HIDLIST_BUILD:=$($(PKG)_DIR)/hid-list
+$(PKG)_BINARY_HIDLIST_TARGET:=$($(PKG)_DEST_DIR)/usr/bin/hid-list
+
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_AURAPEQ
 
 $(PKG_LOCALSOURCE_PACKAGE)
@@ -22,19 +25,30 @@ $($(PKG)_BINARY_BUILD): $($(PKG)_DIR)/.configured
 		-D_DEFAULT_SOURCE -D_GNU_SOURCE \
 		$(AURAPEQ_DIR)/aura-bridged.c \
 		-o $@ \
-		-lhidapi-hidraw -lwebsockets -lpthread
+		-lhidapi-libusb -lwebsockets -lpthread
+
+$($(PKG)_BINARY_HIDLIST_BUILD): $($(PKG)_DIR)/.configured
+	$(MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) $(TARGET_LDFLAGS) \
+		-Wall -Wextra -Os -std=c11 \
+		-D_DEFAULT_SOURCE -D_GNU_SOURCE \
+		$(AURAPEQ_DIR)/hid-list.c \
+		-o $@ \
+		-lhidapi-libusb
 
 $($(PKG)_BINARY_TARGET): $($(PKG)_BINARY_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
+$($(PKG)_BINARY_HIDLIST_TARGET): $($(PKG)_BINARY_HIDLIST_BUILD)
+	$(INSTALL_BINARY_STRIP)
+
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_BINARY_TARGET)
+$(pkg)-precompiled: $($(PKG)_BINARY_TARGET) $($(PKG)_BINARY_HIDLIST_TARGET)
 
 $(pkg)-clean:
-	$(RM) $($(PKG)_BINARY_BUILD)
+	$(RM) $($(PKG)_BINARY_BUILD) $($(PKG)_BINARY_HIDLIST_BUILD)
 
 $(pkg)-uninstall:
-	$(RM) $($(PKG)_BINARY_TARGET)
+	$(RM) $($(PKG)_BINARY_TARGET) $($(PKG)_BINARY_HIDLIST_TARGET)
 
 $(PKG_FINISH)
